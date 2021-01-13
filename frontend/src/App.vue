9c16a408bad4b9c7c56a764ab222e2bc149dadbd
Lukas Huber <hubernikus@gmail.com>
2021-01-13 22:38:47 +0100

    fix sass-induced error

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -345,15 +345,6 @@ html, body {
 <!--     padding: 0; -->
 <!--   } -->
   <!-- </style> -->
-<style lang="scss">
-$font-stack:    Helvetica, sans-serif
-$primary-color: #333
-
-body
-  font: 100% $font-stack
-  color: $primary-color
-</style>
-
 
 <style lang="less">
   html, body {
