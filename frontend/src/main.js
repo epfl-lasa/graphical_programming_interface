ef8840b9855f00de1579d131b5929c2705973de2
Lukas Huber <hubernikus@gmail.com>
2021-01-13 22:33:54 +0100

    remove bootstrat & vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,29 +5,23 @@ import router from './router'
 // import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import Buefy from 'buefy'
+// import 'buefy/dist/buefy.css'
 import 'buefy/dist/buefy.css'
 
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import BootstrapVue from 'bootstrap-vue'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // import VueSlider from 'vue-slider-component'
 // import 'vue-slider-component/theme/default.css'
 
-// Vuetify
-// import vuetify from '@/plugins/vuetify' // path to vuetify export
-// import Vuetify from 'vuetify'
-// import 'vuetify/dist/vuetify.min.css'
-
-// Vue.component('VueSlider', VueSlider) // TODO: remove?
 Vue.use(Buefy)
-Vue.use(BootstrapVue) // TODO: remove bootstrap
-// Vue.use(Vuetify)
-
-Vue.config.productionTip = false
+// Vue.use(BootstrapVue) // TODO: remove bootstrap
 
 // Create IP as global Variable
-Vue.prototype.$localIP = `http://localhost:5000/`
+Vue.prototype.$localIP = `http://localhost:5000`
+
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
