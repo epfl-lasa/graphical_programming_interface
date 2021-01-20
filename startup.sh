58074aee59fa4a045a9a8ead2277bdca64e69143
Lukas Huber <hubernikus@gmail.com>
2021-01-20 15:33:03 +0100

    add startup

---
+++ startup.sh
@@ -0,0 +1,10 @@
+#/usr/bin/bash
+
+## Development mode
+gnome-terminal -e 'sh -c "cd frontend; npm run dev; exec bash"'
+gnome-terminal -e 'sh -c "FLASK_APP=run.py flask run; exec bash"'
+
+# Open code in terminal & keep terminal
+gnome-terminal -e 'sh -c "emacs run.py; exec bash"'
+
+
