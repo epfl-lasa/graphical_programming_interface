8cb0ed2f26e33ff20d80907a7ff9a775bfd66876
Lukas Huber <hubernikus@gmail.com>
2021-03-07 15:22:18 +0100

    add python force listener connected to ros

---
+++ src/backend/python/force_listener.py
@@ -0,0 +1,88 @@
+'''
+AICA user data handler
+
+(c) AICA SarL
+'''
+__author__ = "Lukas Huber"
+__email__ = "lukas@aica.tech"
+
+import time
+
+import numpy as np
+
+# ROS2 utils
+import rclpy
+from rclpy.node import Node
+
+# ROS messages
+from geometry_msgs.msg import WrenchStamped
+
+class ForceListener(Node):
+    def __init__(self, topic="/ft_sensor/netft_data", start_time=0, delta_time_max=5):
+        ''' Force recording'''
+        super().__init__('ros_force_listener')
+        
+        self.topic = topic
+
+        self.time_data = []
+        self.force_data = []
+
+        self.delta_time_max = delta_time_max
+        self.is_active = False
+
+        self.type_magnitude = True
+
+        self.start_time = start_time
+
+        self.subscription_robot_robot = self.create_subscription(
+            WrenchStamped, self.topic, self.callback_msg, 5)
+
+        self.last_update = None
+
+    def reset_data(self):
+        ''' Set graph data to zero. '''
+        self.time_data = []
+        self.force_data = []
+
+    def activate(self, time_sleep=0.1, timeout_time=5):
+        ''' Activate & loop to collect data.'''
+        self.is_active = True
+
+        self.reset_data()
+
+        self.last_update_request = time.time()
+        
+        while self.is_active:
+            rclpy.spin_once(node=self, timeout_sec=time_sleep)
+
+            # Timeout (!)
+            if time.time() - self.last_update_request > timeout_time:
+                print('Node is stopped, since no request recieved.')
+                self.deactivate()
+            
+    def deactivate(self):
+        ''' Deactivate this node. '''
+        self.is_active = False
+
+    def get_updated_data(self):
+        ''' Send data back. '''
+        self.last_update_request = time.time()
+        
+        if len(self.time_data) == 0:
+            return {'time': [], 'data': []}
+        
+        while self.time_data[0] < self.time_data[-1] - self.delta_time_max:
+            del self.time_data[0]
+            del self.force_data[0]
+            
+        # Project time to [-delta_time_max, 0]
+        time_data = [(tt - self.time_data[-1]) for tt in self.time_data]
+        return {'time': time_data, 'data': self.force_data}
+
+    def callback_msg(self, msg, min_delta_time=0.01):
+        ''' Get message '''
+        force = [msg.wrench.force.x, msg.wrench.force.y, msg.wrench.force.z]
+        time_in_sec = msg.header.stamp.sec + msg.header.stamp.nanosec*1e-9
+        
+        self.time_data.append(time_in_sec)
+        self.force_data.append(np.linalg.norm(force))
